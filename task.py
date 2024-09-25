a25374935587fede891c2994444822a3d95935d8
Lewis-lcwc <30075425@lcwc.ac.uk>
2024-09-25 10:11:04 +0000

    tuplestask

--- task.py
+++ task.py
@@ -1,5 +1,12 @@
-def task():
+def task_1(numbers, N): # Numbers is a list of interger values, times is how many times to operate (int)
 
-    # grade should be a tuple containing student name (str), subject (str) and score (int)
+    return numbers
 
-    return grade
+def task_2(N): # N is any interger value
+    msg = ""
+
+    return msg
+
+def task_3(calc): # Calc is a string 
+print(task_1)
+    return result # Result should be a number answer
